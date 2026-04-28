f6d827d0372db6c16f57d4f1fb6136c85c79b7d4
Saksham Mishra <sxm2114@student.bham.ac.uk>
2026-04-29 01:10:57 +0530

    feat: read IMU, BMP, and GPS sensors

--- firmware/cansat/src/main.cpp
+++ firmware/cansat/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <RH_RF69.h>
 #include <SPI.h>
+#include <Adafruit_BMP3XX.h>
+#include <Adafruit_MPU6050.h>
+#include <TinyGPSPlus.h>
 #include "telemetry.h"
 
 #define RFM69_CS PA4
@@ -9,6 +12,13 @@
 #define RF69_FREQ 433.0
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+Adafruit_BMP3XX bmp; 
+Adafruit_MPU6050 mpu;
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(USART2); // PA3 (RX), PA2 (TX) // Check pinouts
+
+float baseline_pressure = 1013.25;
 
 TelemetryPacket pkt;
 uint32_t last_tx = 0;
@@ -20,10 +30,36 @@ void setup() {
 }
 
 void loop() {
+    while (SerialGPS.available() > 0) {
+        gps.encode(SerialGPS.read());
+    }
+
+    if (gps.location.isUpdated() && gps.location.isValid()) {
+        pkt.flags |= 0x04; // gps_fix
+        pkt.lat = gps.location.lat();
+        pkt.lon = gps.location.lng();
+    }
+
     if (millis() - last_tx >= TX_INTERVAL) {
         last_tx = millis();
         pkt.pkt_id++;
         pkt.timestamp_ms = millis();
+        
+        if (pkt.flags & 0x08) {
+            if (bmp.performReading()) {
+                pkt.temp_c = bmp.temperature;
+                pkt.pressure_hpa = bmp.pressure / 100.0;
+                if (pkt.pkt_id == 1) baseline_pressure = pkt.pressure_hpa;
+                pkt.altitude_m = bmp.readAltitude(baseline_pressure);
+            }
+        }
+
+        if (pkt.flags & 0x10) {
+            sensors_event_t a, g, temp;
+            mpu.getEvent(&a, &g, &temp);
+            pkt.accel_z = a.acceleration.z / 9.81; // Convert m/s^2 to G's
+            pkt.gyro_x = g.gyro.x * 57.2958;       // Convert rad/s to deg/s
+        }
         
         // Checksum calculation (XOR bytes 0-35)
         uint8_t* ptr = (uint8_t*)&pkt;
