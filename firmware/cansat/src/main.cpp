74cc0b1dc62df9583a53cb4b4582706eabbd1f21
Saksham Mishra <sxm2114@student.bham.ac.uk>
2026-04-29 01:10:12 +0530

    feat: add RFM69HCW 433MHz transmission

--- firmware/cansat/src/main.cpp
+++ firmware/cansat/src/main.cpp
@@ -1,5 +1,14 @@
 #include <Arduino.h>
+#include <RH_RF69.h>
+#include <SPI.h>
 #include "telemetry.h"
+
+#define RFM69_CS PA4
+#define RFM69_INT PA3
+#define RFM69_RST PA2
+#define RF69_FREQ 433.0
+
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
 TelemetryPacket pkt;
 uint32_t last_tx = 0;
@@ -15,6 +24,21 @@ void loop() {
         last_tx = millis();
         pkt.pkt_id++;
         pkt.timestamp_ms = millis();
+        
+        // Checksum calculation (XOR bytes 0-35)
+        uint8_t* ptr = (uint8_t*)&pkt;
+        pkt.checksum = 0;
+        for(int i = 0; i < 36; i++) {
+            pkt.checksum ^= ptr[i];
+        }
+        
+        // Transmit
+        rf69.send((uint8_t*)&pkt, sizeof(TelemetryPacket));
+        rf69.waitPacketSent();
+        
+        // TODO: SD Write
+    }
+}imestamp_ms = millis();
         
         // Checksum calculation (XOR bytes 0-35)
         uint8_t* ptr = (uint8_t*)&pkt;
